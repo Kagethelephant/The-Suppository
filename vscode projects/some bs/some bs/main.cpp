b58a7be959d16427294ba46f9456f49677c66f03
Kage Palmer <kagethelephant@gmail.com>
2023-11-06 13:35:49 -0700

    add visual for debuging

--- vscode projects/some bs/some bs/main.cpp
+++ vscode projects/some bs/some bs/main.cpp
@@ -5,6 +5,18 @@
 
 int main(int argc, char** argv) {
 
+
+    sf::Vector3i c_black(13, 14, 26);
+    sf::Vector3i c_dkblue(33, 47, 106);
+    sf::Vector3i c_blue(47, 80, 118);
+    sf::Vector3i c_ltblue(70, 113, 128);
+    sf::Vector3i c_tan(116, 113, 89);
+    sf::Vector3i c_green(49, 83, 76);
+    sf::Vector3i c_dkgreen(34, 53, 59);
+    sf::Vector3i c_dkpurple(43, 37, 49);
+    sf::Vector3i c_purple(77, 61, 85);
+    sf::Vector3i c_snow(182, 182, 182);
+
     game res; //this is defined in the header file
     float resW = res.GetResW();
     float resH = res.GetResH();
@@ -17,7 +29,7 @@ int main(int argc, char** argv) {
     int bufferX = bufferW / 2; //center of the buffer texture for positioning the game view
     int bufferY = bufferH / 2;
 
-    int gridSize = 32;
+    int gridSize = 2;
     int gridCountW = round(bufferW / gridSize) + 1;
     int gridCountH = round(bufferH / gridSize) + 1;
 
@@ -42,28 +54,15 @@ int main(int argc, char** argv) {
         float height;
     };
 
-    tile mapGrid[mapSize][mapSize];
-
-    for (int i = 0; i < mapSize; i += 1)
-    {
-        for (int z = 0; z < mapSize; z += 1)
-        {
-            mapGrid[i][z].tileType = "Test";
-            mapGrid[i][z].moisture = 1;
-            mapGrid[i][z].height = 1;
-        }
-    }
-
-
-
     sf::Clock timer; //timer to rotate the hex
     sf::Vector3i col1(50, 104, 240); //we can use a vector to define a color for the background!
 
 
 
-    sf::RenderWindow window(sf::VideoMode(), "Scaling", sf::Style::Fullscreen); //create the winow at default resolution and make fullscreen
+    sf::RenderWindow window(sf::VideoMode(bufferW, bufferH), "Scaling"); //create the winow at default resolution and make fullscreen
 
     sf::View view(sf::Vector2f(bufferX, bufferY), sf::Vector2f(bufferW, bufferH));// create a view and give it the scaled resolution
+    window.setMouseCursorVisible(false);
     window.setView(view); //set the view of the window to the view we just created
 
     sf::RenderTexture buffer; //create the main window texture to draw in the proper resolution (needs to be drawn to the buffer sprite and drawn to the window)
@@ -75,7 +74,7 @@ int main(int argc, char** argv) {
     window.setFramerateLimit(60); //set the frame rate
 
 
-    
+
     sf::Font font;
     sf::Font font1;
     sf::Font font2;
@@ -85,8 +84,8 @@ int main(int argc, char** argv) {
     if (!font2.loadFromFile("../fonts/novem___.ttf")) return 1;
     if (!font3.loadFromFile("../fonts/small_pixel.ttf")) return 1;
 
-    
-    
+
+
     sf::Text text;
     text.setFont(font3);
     text.setString("Hello world");
@@ -94,14 +93,14 @@ int main(int argc, char** argv) {
     text.setFillColor(sf::Color(col1.x, col1.y, col1.z));
     text.setStyle(sf::Text::Regular);
 
-    sf::CircleShape hex(20, 6);
+    sf::CircleShape hex(10, 6);
     hex.setFillColor(sf::Color::Transparent);
     hex.setOutlineColor(sf::Color(col1.x, col1.y, col1.z));
     hex.setOutlineThickness(2);
-    hex.setOrigin(20, 20);
+    hex.setOrigin(10, 10);
 
     sf::RectangleShape rect;
-    rect.setSize(sf::Vector2f(gridSize - 1, gridSize - 1));
+    rect.setSize(sf::Vector2f(gridSize, gridSize));
     rect.setFillColor(sf::Color::Black);
     rect.setOutlineColor(sf::Color::Transparent);
     rect.setOutlineThickness(2);
@@ -114,10 +113,15 @@ int main(int argc, char** argv) {
 
             if (sf::Event::KeyPressed)
             {
-                if (event.key.code == sf::Keyboard::Escape) window.close();
+                if (event.key.code == sf::Keyboard::Escape) { window.close(); }
             }
         }
-        //if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) { window.close();} //this does the same thing as above
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) { playerX += 1; } //this does the same thing as above
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) { playerX -= 1; }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) { playerY += 1; }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) { playerY -= 1; }
+
 
         //UPDATE SHIT
         //create a vector with the mouse position in the window (mapPixelToCoords converts the screen location to the location in the view)
@@ -131,20 +135,34 @@ int main(int argc, char** argv) {
         // Clear the buffer texture to create a background
         buffer.clear(sf::Color(col1.x, col1.y, col1.z));
 
-        for (int i = 0; i < gridCountW; i += 1)
+        playerX = round(mousePos.x / gridSize);
+        playerY = round(mousePos.y / gridSize);
+
+        for (int i = 0; i < 100; i += 1)
         {
-            for (int z = 0; z < gridCountH; z += 1)
+            for (int z = 0; z < 100; z += 1)
             {
+                if (ds.map[i][z] < -10) { rect.setFillColor(sf::Color(c_dkblue.x, c_dkblue.y, c_dkblue.z)); }
+                else if (ds.map[i][z] < -5) { rect.setFillColor(sf::Color(c_blue.x, c_blue.y, c_blue.z)); }
+                else if (ds.map[i][z] < 0) { rect.setFillColor(sf::Color(c_ltblue.x, c_ltblue.y, c_ltblue.z)); }
+                else if (ds.map[i][z] < 5) { rect.setFillColor(sf::Color(c_tan.x, c_tan.y, c_tan.z)); }
+                else if (ds.map[i][z] < 10) { rect.setFillColor(sf::Color(c_green.x, c_green.y, c_green.z)); }
+                else if (ds.map[i][z] < 15) { rect.setFillColor(sf::Color(c_dkgreen.x, c_dkgreen.y, c_dkgreen.z)); }
+                else if (ds.map[i][z] < 20) { rect.setFillColor(sf::Color(c_purple.x, c_purple.y, c_purple.z)); }
+                else { rect.setFillColor(sf::Color(c_dkpurple.x, c_dkpurple.y, c_dkpurple.z)); }
+
+                if (playerX == i && playerY == z) { rect.setFillColor(sf::Color(c_black.x, c_black.y, c_black.z)); }
+
                 rect.setPosition((1) + (i * gridSize), (1) + (z * gridSize));
                 buffer.draw(rect);
-                text.setPosition((1) + (i * gridSize), (1) + (z * gridSize));
-                text.setString(mapGrid[i][z].tileType);
-                buffer.draw(text);
+
             }
         }
 
         buffer.draw(hex);
-
+        //float val = ds.map[playerX][playerY];
+        text.setString("(" + std::to_string(playerX) + ", " + std::to_string(playerY) + ") : " );
+        buffer.draw(text);
         buffer.display(); //send the texture from the back buffer to the screen
         window.draw(bufferSprite);// Draw the render texture's contents
         window.display();// Display the results
--- vscode projects/some bs/some bs/main.h
+++ vscode projects/some bs/some bs/main.h
@@ -25,19 +25,7 @@ public:
 	{
 		return GetSystemMetrics(SM_CYSCREEN);
 	}
-	void colorInit()
-	{
-		sf::Vector3i c_black(13, 14, 26);
-		sf::Vector3i c_dkblue(33, 47, 106);
-		sf::Vector3i c_blue(47, 80, 118);
-		sf::Vector3i c_ltblue(70, 113, 128);
-		sf::Vector3i c_tan(116, 113, 89);
-		sf::Vector3i c_green(49, 83, 76);
-		sf::Vector3i c_dkgreen(34, 53, 59);
-		sf::Vector3i c_dkpurple(43, 37, 49);
-		sf::Vector3i c_purple(77, 61, 85);
-		sf::Vector3i c_snow(182, 182, 182);
-	}
+
 	int randRange(int min, int max)
 	{
 		std::random_device rd; // obtain a random number from hardware
@@ -52,17 +40,21 @@ class DiamSquare
 {
 
 public:
+
+	static const int arraySize = 100;
+	float map[arraySize][arraySize] = {};
+
 	void newMap(int s, int high = 20, float roughness = 20, float change = 1.6)
 	{
 		game rand;
 		const int arraySize = 100;
-		int size = s ^ 2 + 1;
+		int size = pow(s,2) + 1;
 		int chunk = (size); //size of the piece you are working with
 		int half = chunk / 2; // this helps get the center value
 		int low = 0; //bottom end of the random range to set the initial value
-		float empty = -1000;
+		float empty = -1000.0f;
 		//DIAMOND SQUARE ALGORITH this is where the magic happens
-		float map[arraySize][arraySize];
+
 		for (int z = 0; z < size; z += chunk) { for (int i = 0; i < size; i += chunk) { map[i][z] = empty; } }
 
 		//SQUARE
@@ -72,28 +64,15 @@ public:
 			{
 				for (int i = 0; i < size; i += chunk)
 				{
-
-					// 0 - - - 0	This is the square portion of the code the "X" represents
-					// - - - - -	points that have already been populated. these points are 
-					// - - - - -	averaged and a random value is added "roughness" to generate
-					// - - - - -	the "0" values. This is the initialization done above
-					// 0 - - - 0
-
-					// X - 0 - X	This is what the first iteration in this loop will look like
-					// - - - - -	 
-					// 0 - X - 0	
-					// - - - - -	
-					// X - 0 - X
-
-					float x1y1 = *map[i, z]; //grab the "X" values shown above
-					float x2y1 = *map[i + chunk, z];
-					float x1y2 = *map[i, z + chunk];
-					float x2y2 = *map[i + chunk, z + chunk];
+					float x1y1 = map[i][z]; //grab the "X" values shown above
+					float x2y1 = map[i + chunk][z];
+					float x1y2 = map[i][z + chunk];
+					float x2y2 = map[i + chunk][z + chunk];
 
 					float avg = (x1y1 + x2y1 + x1y2 + x2y2) / 4;
 
 					//set the value "0" with the average + a random value and make sure we are not overwriting other values
-					if (*map[i + half, z + half] == empty) *map[i + half, z + half] = avg + rand.randRange(-roughness, roughness);
+					if (map[i + half][z + half] == empty) map[i + half][z + half] = avg + rand.randRange(-roughness, roughness);
 				}
 			}
 
@@ -103,28 +82,16 @@ public:
 				for (int i = (z + half) % chunk; i < size + 1; i += chunk)
 				{
 
-					// X - - - X	This is the diamond portion of the code the "X" represents
-					// - - - - -	points that have already been populated. these points are 
-					// - - 0 - -	averaged and a random value is added "roughness" to generate
-					// - - - - -	the "0" values.
-					// X - - - X
 
-					// X - X - X	This is what the next iteration would look like
-					// - 0 - 0 -	 
-					// X - X - X	
-					// - 0 - 0 -	
-					// X - X - X
-
-
-					float x1y = *map[i - half, z]; //grab the "X" values shown above
-					float x2y = *map[i + half, z];
-					float xy1 = *map[i, z + half];
-					float xy2 = *map[i, z - half];
+					float x1y = map[i - half][z]; //grab the "X" values shown above
+					float x2y = map[i + half][z];
+					float xy1 = map[i][z + half];
+					float xy2 = map[i][z - half];
 
 					float avg = (x1y + x2y + xy1 + xy2) / 4;
 
 					//set the value "0" (from the diagram above) with the average + a random value and make sure we are not overwriting other values
-					if (*map[i, z] == empty) *map[i, z] = avg + rand.randRange(-roughness, roughness);
+					if (map[i][z] == empty) map[i][z] = avg + rand.randRange(-roughness, roughness);
 				}
 			}
 			chunk /= 2; //this is how we itterate through smaller and smaller chunks
