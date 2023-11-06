2d1ed853afd229afd2065f2d4f561539c8e6f59a
Kage Palmer <kagethelephant@gmail.com>
2023-11-06 15:17:47 -0700

    better still

--- vscode projects/some bs/some bs/main.h
+++ vscode projects/some bs/some bs/main.h
@@ -49,20 +49,21 @@ public:
 		game rand;
 		const int arraySize = 100;
 		int size = pow(s,2) + 1;
-		int chunk = (size); //size of the piece you are working with
+		int chunk = (size-1); //size of the piece you are working with
 		int half = chunk / 2; // this helps get the center value
 		int low = 0; //bottom end of the random range to set the initial value
 		float empty = -1000.0f;
 		//DIAMOND SQUARE ALGORITH this is where the magic happens
 
 		for (int z = 0; z < size; z += 1) { for (int i = 0; i < size; i += 1) { map[i][z] = empty; } }
+
 		map[0][0] = 20;
 		map[0][size-1] = 20;
 		map[size-1][0] = 20;
 		map[size-1][size-1] = 20;
 
 		//SQUARE
-		while (chunk > 1) //this is toggled so we can see each iteration
+		while (chunk > 1)
 		{
 			for (int z = 0; z < size; z += chunk)
 			{
@@ -76,25 +77,36 @@ public:
 					float avg = (x1y1 + x2y1 + x1y2 + x2y2) / 4;
 
 					//set the value "0" with the average + a random value and make sure we are not overwriting other values
+					//
 					if (map[i + half][z + half] == empty) map[i + half][z + half] = avg + rand.randRange(-roughness, roughness);
 				}
 			}
 
 			//DIAMOND
-			for (int z = 0; z <= size + 1; z += half)
+			for (int z = 0; z < size+1; z += half)
 			{
-				for (int i = (z + half) % chunk; i <= size + 1; i += chunk)
+				for (int i = (z + half) % chunk; i < size+1; i += chunk)
 				{
 
+					float x1y = 0;
+					float x2y = 0;
+					float xy1 = 0;
+					float xy2 = 0;
 
-					float x1y = map[i - half][z]; //grab the "X" values shown above
-					float x2y = map[i + half][z];
-					float xy1 = map[i][z + half];
-					float xy2 = map[i][z - half];
+					int cnt = 4; 
+					if (map[i - half][z]) { x1y = map[i - half][z]; } //grab the "X" values shown above
+					else { cnt -= 1; }
+					if (map[i + half][z]) { x2y = map[i + half][z]; }
+					else { cnt -= 1; }
+					if (map[i][z + half]) { xy1 = map[i][z + half]; }
+					else { cnt -= 1; }
+					if (map[i][z - half]) { xy2 = map[i][z - half]; }
+					else { cnt -= 1; }
 
-					float avg = (x1y + x2y + xy1 + xy2) / 4;
+					float avg = (x1y + x2y + xy1 + xy2) / cnt;
 
 					//set the value "0" (from the diagram above) with the average + a random value and make sure we are not overwriting other values
+					//
 					if (map[i][z] == empty) map[i][z] = avg + rand.randRange(-roughness, roughness);
 				}
 			}
