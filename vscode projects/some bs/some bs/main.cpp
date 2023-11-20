1856ec120e2aa6fb60a44edffdb5fd4c433c9708
Kagethelephant <kagethelephant@gmail.com>
2023-11-20 16:52:03 -0700

    draw tileset from png image using vertex array

--- vscode projects/some bs/some bs/main.cpp
+++ vscode projects/some bs/some bs/main.cpp
@@ -11,42 +11,46 @@ int main(int argc, char** argv) {
     //----GRID STUFF----
 
     //size in pixels of the grids to draw to the window
-    int gridSize = 4;
+    int tileSize = 16;
+
     //size of diamond square map *MUST BE EVEN*
-
-    int mouseX = 150;
-    int mouseY = 150;
-
+    sf::Vector2i mousePos;
+    sf::Vector2i mouseGrid;
     sf::Vector2i viewPos;
 
+    //start position for the view
     viewPos.x = 100;
     viewPos.y = 125;
+
+
 
     //Create the diamond square object and run function to generate map
     DiamSquare ds;
     ds.newMap(map, mapSize);
 
 
-    //----VIEW SETUP/SFML----
 
 
     //----VIEW SETUP/SFML----
 
     sf::RenderWindow window; 
     sf::View view;
-    sf::RenderTexture buffer;
-    sf::Sprite bufferSprite;
 
-    Game win;
-    sf::Vector2i res;
-    res = win.windowSetup(window, view, buffer, bufferSprite, 450);
+    game win;  
+
+    //container for the height and width of the window
+    sf::Vector2i resPixels;
+    resPixels = win.windowSetup(window, view, 600, true,60);
 
     //create a seperate texture to draw the GUI to
-    sf::RenderTexture bufferGUI; 
-    bufferGUI.create(res.x, res.y); 
+    sf::RenderTexture bufferMap;
+    sf::RenderTexture bufferGUI;
+
+    bufferMap.create(resPixels.x, resPixels.y);
+    bufferGUI.create(resPixels.x, resPixels.y);
+
+    sf::Sprite bufferSpriteMap(bufferMap.getTexture());
     sf::Sprite bufferSpriteGUI(bufferGUI.getTexture());
-
-
 
 
 
@@ -55,54 +59,41 @@ int main(int argc, char** argv) {
     //----GLYPHS----
 
     //load font
-    sf::Font font;
-    if (!font.loadFromFile("../fonts/small_pixel.ttf")) return 1;
+    sf::Font fontSmall;
+    if (!fontSmall.loadFromFile("../fonts/small_pixel.ttf")) return 1;
     
     //Text for the cursor position and debugging
-    sf::Text text;
-    text.setFont(font);
-    text.setString("Hello world");
-    text.setCharacterSize(8);
-    text.setFillColor(sf::Color(c_black.x, c_black.y, c_black.z));
-    text.setStyle(sf::Text::Regular);
-    text.setPosition(5, 5);
-
-    //Hexagon for cursor
-    sf::CircleShape hex(10, 6);
-    hex.setFillColor(sf::Color::Transparent);
-    hex.setOutlineColor(sf::Color(c_black.x, c_black.y, c_black.z));
-    hex.setOutlineThickness(2);
-    hex.setOrigin(10, 10);
+    sf::Text textSmall;
+    textSmall.setFont(fontSmall);
+    textSmall.setString("Hello world");
+    textSmall.setCharacterSize(8);
+    textSmall.setFillColor(sf::Color(c_black.x, c_black.y, c_black.z));
+    textSmall.setStyle(sf::Text::Regular);
+    textSmall.setPosition(5, 5);
 
     //rectangles to draw the grids
-    sf::RectangleShape rect;
-    rect.setSize(sf::Vector2f(gridSize, gridSize));
-    rect.setFillColor(sf::Color::Transparent);
-    rect.setOutlineColor(sf::Color::Black);
-    rect.setOutlineThickness(2);
-    rect.setOrigin(0, 0);
+    sf::RectangleShape rectCursor;
+    rectCursor.setSize(sf::Vector2f(tileSize, tileSize));
+    rectCursor.setFillColor(sf::Color::Transparent);
+    rectCursor.setOutlineColor(sf::Color::Black);
+    rectCursor.setOutlineThickness(2);
+    rectCursor.setOrigin(0, 0);
 
-
-    //----DRAW STATIC----
 
 
 
     //----DRAW STATIC----
 
     //how many grids can fit on the screen
-    int viewGridW = floor(res.x / gridSize);
-    int viewGridH = floor(res.y / gridSize);
-    int viewGridX = viewPosX - floor((viewGridW)/2);
-    int viewGridY = viewPosY - floor((viewGridH)/2);
+    sf::Vector2i resTiles;
+    resTiles.x = floor(resPixels.x / tileSize);
+    resTiles.y = floor(resPixels.y / tileSize);
 
-    sf::Vector2u tileSize(gridSize, gridSize);
 
     //clear viewwith a background color
-    buffer.clear(sf::Color(c_black.x, c_black.y, c_black.z));
+    bufferMap.clear(sf::Color(c_black.x, c_black.y, c_black.z));
 
-    ds.load(buffer, map, tileSize,viewPos, viewGridW, viewGridH);
-
-
+    ds.drawMap(bufferMap, map, tileSize,viewPos, resTiles,false,"../sprites/tileset.png");
 
 
 
@@ -136,77 +127,88 @@ int main(int argc, char** argv) {
         }
 
 
+
+
         //----UPDATE----
 
         // regenerate the map when the enter button is pressed
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
         {
             ds.newMap(map, mapSize);
-            ds.load(buffer, map, tileSize, viewPos, viewGridW, viewGridH);
+            ds.drawMap(bufferMap, map, tileSize, viewPos, resTiles, false, "../sprites/tileset.png");
         }
+
+
+
+
+
 
         //move the map around and update the graphics
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         {
             viewPos.x += 1;
-            if (viewPos.x > mapSize - floor((viewGridW) / 2)) viewPos.x = mapSize - floor((viewGridW) / 2);
+            if (viewPos.x > mapSize - floor((resTiles.x) / 2)) viewPos.x = mapSize - floor((resTiles.x) / 2);
             else
             {
-                ds.load(buffer, map, tileSize, viewPos, viewGridW, viewGridH);
+                ds.drawMap(bufferMap, map, tileSize, viewPos, resTiles, false, "../sprites/tileset.png");
             }
         }
+
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
             viewPos.x -= 1;
-            if (viewPos.x < floor((viewGridW) / 2)+1) viewPos.x = floor((viewGridW) / 2) + 1;
+            if (viewPos.x < floor((resTiles.x) / 2)+1) viewPos.x = floor((resTiles.x) / 2)+1;
             else
             {
-                ds.load(buffer, map, tileSize, viewPos, viewGridW, viewGridH);
+                ds.drawMap(bufferMap, map, tileSize, viewPos, resTiles, false, "../sprites/tileset.png");
             }
         }
+
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         {
             viewPos.y -= 1;
-            if (viewPos.y < floor((viewGridH) / 2)+1) viewPos.y = floor((viewGridH) / 2) + 1;
+            if (viewPos.y < floor((resTiles.y) / 2)+1) viewPos.y = floor((resTiles.y) / 2)+1;
             else
             {
-                ds.load(buffer, map, tileSize, viewPos, viewGridW, viewGridH);
+                ds.drawMap(bufferMap, map, tileSize, viewPos, resTiles, false, "../sprites/tileset.png");
             }
         }
+
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
         {
             viewPos.y += 1;
-            if (viewPos.y > mapSize - floor((viewGridH) / 2)) viewPos.y = mapSize - floor((viewGridH) / 2);
+            if (viewPos.y > mapSize - floor((resTiles.y) / 2)-1) viewPos.y = mapSize - floor((resTiles.y) / 2)-1;
             else
             {
-                ds.load(buffer, map, tileSize, viewPos, viewGridW, viewGridH);
+                ds.drawMap(bufferMap, map, tileSize, viewPos, resTiles, false, "../sprites/tileset.png");
             }
         } 
         
 
 
+
+
         //----UPDATE MAKE CALCULATIONS-----
         
         //update the map position to give accurate grid positions
-        int viewGridX = viewPos.x - floor((viewGridW) / 2);
-        int viewGridY = viewPos.y - floor((viewGridH) / 2);
+        int viewGridX = viewPos.x - floor((resTiles.x) / 2);
+        int viewGridY = viewPos.y - floor((resTiles.y) / 2);
 
         //Create a vector with the pixel coord's in the actual window not the display
         sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
 
-        //Update the positon of the hex (this is the cursor for now)
-        hex.setPosition(mousePos.x + 3, mousePos.y + 3); 
 
         //This is the position of our mouse in world grid coord's but keep it in the map
-        mouseX = round(mousePos.x / gridSize);
-        mouseY = round(mousePos.y / gridSize);
-        if (mouseX > mapSize) mouseX = mapSize;
-        if (mouseY > mapSize) mouseY = mapSize;
-        if (mouseX < 0) mouseX = 0;
-        if (mouseY < 0) mouseY = 0;
+        mouseGrid.x = round(mousePos.x / tileSize);
+        mouseGrid.y = round(mousePos.y / tileSize);
+
+        if (mouseGrid.x > mapSize) mouseGrid.x = mapSize;
+        if (mouseGrid.y > mapSize) mouseGrid.y = mapSize;
+        if (mouseGrid.x < 0) mouseGrid.x = 0;
+        if (mouseGrid.y < 0) mouseGrid.y = 0;
 
 
 
@@ -214,33 +216,17 @@ int main(int argc, char** argv) {
         //----DRAW UPDATE-----
       
         //clear the view transparent so it doesnt cover up the main buffer!
-        bufferGUI.clear(sf::Color(0,0,0,0));
-
-        //Draw the cursor on the screen
-        bufferGUI.draw(hex);
+        bufferGUI.clear(sf::Color::Transparent);
 
         //Draw the coord of the mouse on the screen as long as its in the map  
-        int newX = mouseX + viewGridX;
-        int newY = mouseY + viewGridY;
-        text.setString("(" + std::to_string(newX) + ", " + std::to_string(newY) + ") : " + std::to_string(map[newX][newY]));
-        bufferGUI.draw(text);
+        int newX = mouseGrid.x + viewGridX;
+        int newY = mouseGrid.y + viewGridY;
+        textSmall.setString("(" + std::to_string(newX) + ", " + std::to_string(newY) + ") : " + std::to_string(map[newX][newY]));
+        bufferGUI.draw(textSmall);
 
         //Move the rectangle to the correct position before drawing
-        rect.setPosition(mouseX*gridSize, mouseY*gridSize);
-        bufferGUI.draw(rect);
-
-
-        sf::VertexArray f(sf::Triangles, 3);
-        f[0].position = sf::Vector2f(20.0f, 20.0f);
-        f[0].color = sf::Color(c_blue.x, c_blue.y, c_blue.z);
-
-        f[1].position = sf::Vector2f(100.0f, 20.0f);
-        f[1].color = sf::Color::Red;
-
-        f[2].position = sf::Vector2f(50.0f, 50.0f);
-        f[2].color = sf::Color::Green;
-
-        buffer.draw(f);
+        rectCursor.setPosition(mouseGrid.x*tileSize, mouseGrid.y*tileSize);
+        bufferGUI.draw(rectCursor);
 
 
 
@@ -249,8 +235,8 @@ int main(int argc, char** argv) {
         
         //display the buffer on to the window and draw the sprite 
         //on to the window (not really sure how this works honestly)
-        buffer.display(); 
-        window.draw(bufferSprite);
+        bufferMap.display(); 
+        window.draw(bufferSpriteMap);
 
         //do the same as above but for the GUI
         bufferGUI.display(); 
@@ -261,9 +247,10 @@ int main(int argc, char** argv) {
     }
 
 
+
     //----DEBUGGING OUTPUTS----
 
-    std::cout << "Window Resolution:  " << res.x << " X " << res.y << std::endl;
+    std::cout << "Window Resolution:  " << resPixels.x << " X " << resPixels.y << std::endl;
     std::cout << std::endl;
 
     return 0;
--- vscode projects/some bs/some bs/main.hpp
+++ vscode projects/some bs/some bs/main.hpp
@@ -26,7 +26,7 @@ sf::Vector3i c_purple(77, 61, 85);
 sf::Vector3i c_snow(182, 182, 182);
 
 //alocate space for the grid
-static const int mapSize = 1000;
+static const int mapSize = 500;
 //create the grid here so it stays in the heap
 float map[mapSize][mapSize] = { -1000.0f };
 
@@ -52,7 +52,7 @@ public:
 
 	//----INITIALIZE VIEW----
 
-	sf::Vector2i windowSetup(sf::RenderWindow &win, sf::View &vw, sf::RenderTexture &buf, sf::Sprite &spr, int h)
+	sf::Vector2i windowSetup(sf::RenderWindow &win, sf::View &vw, int h, bool fullscreen = true, int frame = 60)
 	{
 		//get the display dimmensions and calculate the aspect ratio
 		float displayWidth = GetSystemMetrics(SM_CXSCREEN);
@@ -68,7 +68,8 @@ public:
 		int bufferY = bufferH / 2;
 
 		//initialize the view with the calculated resolution 
-		win.create(sf::VideoMode(bufferW, bufferH), "Some BS", sf::Style::Fullscreen);
+		if(fullscreen) win.create(sf::VideoMode(bufferW, bufferH), "Some BS", sf::Style::Fullscreen);
+		else win.create(sf::VideoMode(bufferW, bufferH), "Some BS", sf::Style::Default);
 		//set the frame rate and hide the cursor so we can draw our own
 		win.setFramerateLimit(60);
 		win.setMouseCursorVisible(false);
@@ -79,10 +80,6 @@ public:
 
 		//asign the view to the window
 		win.setView(vw);
-
-		//create a buffer and sprite to draw to
-		buf.create(bufferW, bufferH);
-		spr.setTexture(buf.getTexture());
 
 		//return the calculated width and height of the window in pixels
 		return sf::Vector2i(bufferW, bufferH);
@@ -102,7 +99,7 @@ public:
 
 		//---INITIALIZE VARIABLES----
 
-		Game rand;
+		game rand;
 		float avg;
 		int div;
 
@@ -234,9 +231,11 @@ public:
 					if (map[(int)round(i)][(int)round(z)] == empty) map[(int)round(i)][(int)round(z)] = avg + rand.randRange(-roughness, roughness);
 				}
 			}
+
 			//Create a smaller chunk to iterate through and use half to get the center again
 			chunk /= 2; 
 			half /= 2;
+
 			//reduce the amount of change each iteration (higher value is smoother because its reducing the change faster)
 			roughness /= change; 
 		}
@@ -249,64 +248,95 @@ public:
 
 	//---DRAW WITH VERTICES---
 
-	bool load(sf::RenderTarget& target, float map[mapSize][mapSize], sf::Vector2u tileSize, sf::Vector2i pos, unsigned int width, unsigned int height)
+	bool drawMap(sf::RenderTarget& target, float map[mapSize][mapSize], int tileSize, sf::Vector2i pos, sf::Vector2i size,bool solidColor = true, const std::string& tileset = "NULL")
 	{
 		// load the tileset texture
-		//if (!m_tileset.loadFromFile(tileset))
-		//	return false;
+		if (!solidColor)
+		{
+			if (!m_tileset.loadFromFile(tileset))
+				return false;
+		}
+		
 
 		// resize the vertex array to fit the level size
 		m_vertices.setPrimitiveType(sf::Triangles);
-		m_vertices.resize(width * height * 6);
-		int offsetX = pos.x - ((int)floor(width / 2));
-		int offsetY = pos.y - ((int)floor(height / 2));
+		m_vertices.resize(size.x * size.y * 6);
+
+
+		int offsetX = pos.x - ((int)floor(size.x / 2));
+		int offsetY = pos.y - ((int)floor(size.y / 2));
+
 
 		// populate the vertex array, with two triangles per tile
-		for (unsigned int i = 0 ; i < width; ++i)
-			for (unsigned int j = 0; j < height; ++j)
+		for (unsigned int i = 0 ; i < size.x; ++i)
+			for (unsigned int j = 0; j < size.y; ++j)
 			{
 				// get the current tile number
-				int tileNumber = map[i+offsetX][j + offsetY];
-				sf::Color color;
-				// find its position in the tileset texture
-				//int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
-				//int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
-
-				if (tileNumber < -10) { color = (sf::Color(c_dkblue.x, c_dkblue.y, c_dkblue.z)); }
-				else if (tileNumber < -5) { color = (sf::Color(c_blue.x, c_blue.y, c_blue.z)); }
-				else if (tileNumber < 0) { color = (sf::Color(c_ltblue.x, c_ltblue.y, c_ltblue.z)); }
-				else if (tileNumber < 5) { color = (sf::Color(c_tan.x, c_tan.y, c_tan.z)); }
-				else if (tileNumber < 10) { color = (sf::Color(c_green.x, c_green.y, c_green.z)); }
-				else if (tileNumber < 15) { color = (sf::Color(c_dkgreen.x, c_dkgreen.y, c_dkgreen.z)); }
-				else if (tileNumber < 20) { color = (sf::Color(c_purple.x, c_purple.y, c_purple.z)); }
-				else { color = (sf::Color(c_dkpurple.x, c_dkpurple.y, c_dkpurple.z)); }
-
+				int tileNumber = map[i + offsetX][j + offsetY];
 
 				// get a pointer to the triangles' vertices of the current tile
-				sf::Vertex* triangles = &m_vertices[(i + j * width) * 6];
+				sf::Vertex* triangles = &m_vertices[(i + j * size.x) * 6];
 
 				// define the 6 corners of the two triangles
-				triangles[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
-				triangles[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-				triangles[2].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
-				triangles[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
-				triangles[4].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
-				triangles[5].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+				triangles[0].position = sf::Vector2f(i * tileSize, j * tileSize);
+				triangles[1].position = sf::Vector2f((i + 1) * tileSize, j * tileSize);
+				triangles[2].position = sf::Vector2f(i * tileSize, (j + 1) * tileSize);
+				triangles[3].position = sf::Vector2f(i * tileSize, (j + 1) * tileSize);
+				triangles[4].position = sf::Vector2f((i + 1) * tileSize, j * tileSize);
+				triangles[5].position = sf::Vector2f((i + 1) * tileSize, (j + 1) * tileSize);
 
-				// define the 6 matching texture coordinates
-				//triangles[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
-				//triangles[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-				//triangles[2].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-				//triangles[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
-				//triangles[4].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
-				//triangles[5].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
-
-				for (int k = 0; k < 6; k += 1)
+				if (solidColor)
 				{
-					triangles[k].color = sf::Color(color);
+
+					sf::Color color;
+
+					if (tileNumber < -10) { color = (sf::Color(c_dkblue.x, c_dkblue.y, c_dkblue.z)); }
+					else if (tileNumber < -5) { color = (sf::Color(c_blue.x, c_blue.y, c_blue.z)); }
+					else if (tileNumber < 0) { color = (sf::Color(c_ltblue.x, c_ltblue.y, c_ltblue.z)); }
+					else if (tileNumber < 5) { color = (sf::Color(c_tan.x, c_tan.y, c_tan.z)); }
+					else if (tileNumber < 10) { color = (sf::Color(c_green.x, c_green.y, c_green.z)); }
+					else if (tileNumber < 15) { color = (sf::Color(c_dkgreen.x, c_dkgreen.y, c_dkgreen.z)); }
+					else if (tileNumber < 20) { color = (sf::Color(c_purple.x, c_purple.y, c_purple.z)); }
+					else { color = (sf::Color(c_dkpurple.x, c_dkpurple.y, c_dkpurple.z)); }
+
+					for (int k = 0; k < 6; k += 1)
+					{
+						triangles[k].color = sf::Color(color);
+					}
 				}
+
+				
+				else
+				{
+					int index;
+					// get the current tile number
+					if (tileNumber < -10) { index = 0; }
+					else if (tileNumber < -5) { index = 1; }
+					else if (tileNumber < 0) { index = 2; }
+					else if (tileNumber < 5) { index = 3; }
+					else if (tileNumber < 10) { index = 4; }
+					else if (tileNumber < 15) { index = 5; }
+					else if (tileNumber < 20) { index = 6; }
+					else { index = 7; }
+
+					// find its position in the tileset texture
+					int tu = index % (m_tileset.getSize().x / tileSize);
+					int tv = index / (m_tileset.getSize().x / tileSize);
+
+					// define the 6 matching texture coordinates
+					triangles[0].texCoords = sf::Vector2f(tu * tileSize, tv * tileSize);
+					triangles[1].texCoords = sf::Vector2f((tu + 1) * tileSize, tv * tileSize);
+					triangles[2].texCoords = sf::Vector2f(tu * tileSize, (tv + 1) * tileSize);
+					triangles[3].texCoords = sf::Vector2f(tu * tileSize, (tv + 1) * tileSize);
+					triangles[4].texCoords = sf::Vector2f((tu + 1) * tileSize, tv * tileSize);
+					triangles[5].texCoords = sf::Vector2f((tu + 1) * tileSize, (tv + 1) * tileSize);
+
+				}
+				
 			}
-		target.draw(m_vertices);
+
+		if(solidColor)target.draw(m_vertices);
+		else target.draw(m_vertices, &m_tileset); //Need to include the texture???
 		return true;
 	}
 
@@ -318,13 +348,13 @@ private:
 		states.transform *= getTransform();
 
 		// apply the tileset texture
-		/*states.texture = &m_tileset;*/
+		states.texture = &m_tileset;
 
 		// draw the vertex array
 		target.draw(m_vertices, states);
 	}
 
 	sf::VertexArray m_vertices;
-	/*sf::Texture m_tileset;*/
+	sf::Texture m_tileset;
 };
 	
