ff9b114ba3271d01b7f82dd7eb62e180051ce462
Kagethelephant <kagethelephant@gmail.com>
2023-12-14 16:43:01 -0700

    troubleshooting

--- vscode projects/some bs/some bs/map.cpp
+++ vscode projects/some bs/some bs/map.cpp
@@ -50,154 +50,152 @@ Map::~Map()
 
 void Map::newMap(int _high, float _roughness, float _change)
 {
+
+	//---INITIALIZE VARIABLES----
+
+	float avg;
+	int div;
+
+	//points to extrapolate from for square step
+	float x1y1;
+	float x2y1;
+	float x1y2;
+	float x2y2;
+
+	//points to extrapolate from for diamond step
+	float x1y;
+	float x2y;
+	float xy1;
+	float xy2;
+
+	//create a chunk the size of the grid that will be halfed every itteration
+	float chunk = m_mapSize;
+	//to find the center of the chunk we are working with
+	float half = m_mapSize / 2;
+	//to initialize the grid so we know if we missed one
+	float empty = -1000.0f;
+
+
+
+	//---INITIALIZE MAP----
+
+	//initialize the grid (1 cell grater then the size in the x and y ge
+	for (int z = 0; z < m_mapSize; ++z)
 	{
-
-		//---INITIALIZE VARIABLES----
-
-		float avg;
-		int div;
-
-		//points to extrapolate from for square step
-		float x1y1;
-		float x2y1;
-		float x1y2;
-		float x2y2;
-
-		//points to extrapolate from for diamond step
-		float x1y;
-		float x2y;
-		float xy1;
-		float xy2;
-
-		//create a chunk the size of the grid that will be halfed every itteration
-		float chunk = m_mapSize;
-		//to find the center of the chunk we are working with
-		float half = m_mapSize / 2;
-		//to initialize the grid so we know if we missed one
-		float empty = -1000.0f;
-
-
-
-		//---INITIALIZE MAP----
-
-		//initialize the grid (1 cell grater then the size in the x and y ge
-		for (int z = 0; z < m_mapSize; ++z)
+		for (int i = 0; i < m_mapSize; ++i)
 		{
-			for (int i = 0; i < m_mapSize; ++i)
+			m_map[i][z] = empty;
+		}
+	}
+
+	//set initial corner values to extrapolate from at each corner
+	m_map[0][0] = randRange(-_roughness, _roughness);
+	m_map[0][m_mapSize] = randRange(-_roughness, _roughness);
+	m_map[m_mapSize][0] = randRange(-_roughness, _roughness);
+	m_map[m_mapSize][m_mapSize] = randRange(-_roughness, _roughness);
+
+
+	//----SQUARE-----
+
+	while (chunk > 1)
+	{
+		for (float z = 0; z < m_mapSize; z += chunk)
+		{
+			for (float i = 0; i < m_mapSize; i += chunk)
 			{
-				m_map[i][z] = empty;
+
+				// 0 - - - 0	This is the square portion of the code the "X" represents
+				// - - - - -	points that have already been populated. these points are 
+				// - - - - -	averaged and a random value is added "roughness" to generate
+				// - - - - -	the "0" values. This is the initialization done above
+				// 0 - - - 0
+
+				// X - 0 - X	This is what the first iteration in this loop will look like
+				// - - - - -	 
+				// 0 - X - 0	
+				// - - - - -	
+				// X - 0 - X
+
+
+				//grab the "X" values shown above
+				x1y1 = m_map[(int)round(i)][(int)round(z)];
+				x2y1 = m_map[(int)round(i + chunk)][(int)round(z)];
+				x1y2 = m_map[(int)round(i)][(int)round(z + chunk)];
+				x2y2 = m_map[(int)round(i + chunk)][(int)round(z + chunk)];
+
+				avg = (x1y1 + x2y1 + x1y2 + x2y2) / 4;
+
+				//set the value "0" with the average + a random value and make sure we are not overwriting other values
+				if (m_map[(int)round(i + half)][(int)round(z + half)] == empty) m_map[(int)round(i + half)][(int)round(z + half)] = avg + randRange(-_roughness, _roughness);
 			}
 		}
 
-		//set initial corner values to extrapolate from at each corner
-		m_map[0][0] = randRange(-_roughness, _roughness);
-		m_map[0][m_mapSize] = randRange(-_roughness, _roughness);
-		m_map[m_mapSize][0] = randRange(-_roughness, _roughness);
-		m_map[m_mapSize][m_mapSize] = randRange(-_roughness, _roughness);
 
 
-		//----SQUARE-----
+		//----DIAMOND----
 
-		while (chunk > 1)
+		for (float z = 0; z < m_mapSize + 1; z += half)
 		{
-			for (float z = 0; z < m_mapSize; z += chunk)
+			for (float i = fmodf((z + half), chunk); i < m_mapSize + 1; i += chunk)
 			{
-				for (float i = 0; i < m_mapSize; i += chunk)
+				// X - - - X	This is the diamond portion of the code the "X" represents
+				// - - - - -	points that have already been populated. these points are 
+				// - - 0 - -	averaged and a random value is added "roughness" to generate
+				// - - - - -	the "0" values.
+				// X - - - X
+
+				// X - X - X	This is what the next iteration would look like
+				// - 0 - 0 -	 
+				// X - X - X	
+				// - 0 - 0 -	
+				// X - X - X
+
+				x1y = 0;
+				x2y = 0;
+				xy1 = 0;
+				xy2 = 0;
+
+				div = 0;
+
+				//grab the "X" values shown above if they are in the map bounds
+				if (i - half >= 0)
 				{
-
-					// 0 - - - 0	This is the square portion of the code the "X" represents
-					// - - - - -	points that have already been populated. these points are 
-					// - - - - -	averaged and a random value is added "roughness" to generate
-					// - - - - -	the "0" values. This is the initialization done above
-					// 0 - - - 0
-
-					// X - 0 - X	This is what the first iteration in this loop will look like
-					// - - - - -	 
-					// 0 - X - 0	
-					// - - - - -	
-					// X - 0 - X
-
-
-					//grab the "X" values shown above
-					x1y1 = m_map[(int)round(i)][(int)round(z)];
-					x2y1 = m_map[(int)round(i + chunk)][(int)round(z)];
-					x1y2 = m_map[(int)round(i)][(int)round(z + chunk)];
-					x2y2 = m_map[(int)round(i + chunk)][(int)round(z + chunk)];
-
-					avg = (x1y1 + x2y1 + x1y2 + x2y2) / 4;
-
-					//set the value "0" with the average + a random value and make sure we are not overwriting other values
-					if (m_map[(int)round(i + half)][(int)round(z + half)] == empty) m_map[(int)round(i + half)][(int)round(z + half)] = avg + randRange(-_roughness, _roughness);
+					x1y = m_map[(int)round(i - half)][(int)round(z)];
+					div += 1;
 				}
-			}
 
-
-
-			//----DIAMOND----
-
-			for (float z = 0; z < m_mapSize +1; z += half)
-			{
-				for (float i = fmodf((z + half), chunk); i < m_mapSize +1; i += chunk)
+				if (i + half < m_mapSize + 1)
 				{
-					// X - - - X	This is the diamond portion of the code the "X" represents
-					// - - - - -	points that have already been populated. these points are 
-					// - - 0 - -	averaged and a random value is added "roughness" to generate
-					// - - - - -	the "0" values.
-					// X - - - X
-
-					// X - X - X	This is what the next iteration would look like
-					// - 0 - 0 -	 
-					// X - X - X	
-					// - 0 - 0 -	
-					// X - X - X
-
-					x1y = 0;
-					x2y = 0;
-					xy1 = 0;
-					xy2 = 0;
-
-					div = 0;
-
-					//grab the "X" values shown above if they are in the map bounds
-					if (i - half >= 0)
-					{
-						x1y = m_map[(int)round(i - half)][(int)round(z)];
-						div += 1;
-					}
-
-					if (i + half < m_mapSize + 1)
-					{
-						x2y = m_map[(int)round(i + half)][(int)round(z)];
-						div += 1;
-					}
-
-					if (z + half < m_mapSize + 1)
-					{
-						xy1 = m_map[(int)round(i)][(int)round(z + half)];
-						div += 1;
-					}
-
-					if (z - half >= 0)
-					{
-						xy2 = m_map[(int)round(i)][(int)round(z - half)];
-						div += 1;
-					}
-
-					//calculate the average and use div to only average the amount of values grabbed
-					avg = (x1y + x2y + xy1 + xy2) / 4;
-
-					//set the value "0" (from the diagram above) with the average + a random value and make sure we are not overwriting other values
-					if (m_map[(int)round(i)][(int)round(z)] == empty) m_map[(int)round(i)][(int)round(z)] = avg + randRange(-_roughness, _roughness);
+					x2y = m_map[(int)round(i + half)][(int)round(z)];
+					div += 1;
 				}
+
+				if (z + half < m_mapSize + 1)
+				{
+					xy1 = m_map[(int)round(i)][(int)round(z + half)];
+					div += 1;
+				}
+
+				if (z - half >= 0)
+				{
+					xy2 = m_map[(int)round(i)][(int)round(z - half)];
+					div += 1;
+				}
+
+				//calculate the average and use div to only average the amount of values grabbed
+				avg = (x1y + x2y + xy1 + xy2) / 4;
+
+				//set the value "0" (from the diagram above) with the average + a random value and make sure we are not overwriting other values
+				if (m_map[(int)round(i)][(int)round(z)] == empty) m_map[(int)round(i)][(int)round(z)] = avg + randRange(-_roughness, _roughness);
 			}
-
-			//Create a smaller chunk to iterate through and use half to get the center again
-			chunk /= 2;
-			half /= 2;
-
-			//reduce the amount of change each iteration (higher value is smoother because its reducing the change faster)
-			_roughness /= _change;
 		}
+
+		//Create a smaller chunk to iterate through and use half to get the center again
+		chunk /= 2;
+		half /= 2;
+
+		//reduce the amount of change each iteration (higher value is smoother because its reducing the change faster)
+		_roughness /= _change;
 	}
 
 }
